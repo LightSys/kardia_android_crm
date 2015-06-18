60145ba755ee7335d97c84ab223393617187ff4b
jakeprem <jakeprem@gmail.com>
2015-06-18 17:28:48 -0600

    Worked on the Database and user authentication.

--- app/src/main/java/org/lightsys/crmapp/LoginActivity.java
+++ app/src/main/java/org/lightsys/crmapp/LoginActivity.java
@@ -1,22 +1,68 @@
 package org.lightsys.crmapp;
 
+import android.database.Cursor;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.lightsys.crmapp.data.Account;
+import org.lightsys.crmapp.data.LocalDatabaseHelper;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 
 public class LoginActivity extends ActionBarActivity {
 
+    ListView accountsList;
+    EditText accountName, accountPassword, serverAddress;
+    TextView connectedAccounts;
+    Toolbar toolbar;
+    //private ArrayList<Account> accounts;
+    Cursor accounts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.mainToolbar);
-        setSupportActionBar(toolbar);
-
         setContentView(R.layout.activity_login);
+
+        toolbar = (Toolbar) findViewById(R.id.loginToolbar);
+        setSupportActionBar(toolbar);
+        // This should be centered, it seems that will require creating a custom view using XML.
+        getSupportActionBar().setTitle("Kardia CRM");
+        getSupportActionBar().setSubtitle("Login");
+
+        accountName = (EditText) findViewById(R.id.loginUsername);
+        accountPassword = (EditText) findViewById(R.id.loginPassword);
+        serverAddress = (EditText) findViewById(R.id.loginServer);
+        connectedAccounts = (TextView) findViewById(R.id.loginConnectedAccounts);
+
+        loadAccounts();
+
+
+    }
+
+    public void loadAccounts() {
+        LocalDatabaseHelper db = new LocalDatabaseHelper(this);
+
+        accounts = db.getAccounts();
+
+        if (accounts.getCount() > 0) {
+            connectedAccounts.setText("Connected Accounts:");
+        }
+        else {
+            connectedAccounts.setText("");
+        }
+
+        // Logic to display the returned accounts in a ListView. The ListView will need to be added to
+        // the XML activity_login, and then a Cursor adapter will need to be used.
+        // Looks like a loader might be a better option than a cursor adapter. Or I could go simple
+        // and use an ArrayList and forgo the adapter altogether.
     }
 
     @Override
--- app/src/main/java/org/lightsys/crmapp/data/LocalDatabaseHelper.java
+++ app/src/main/java/org/lightsys/crmapp/data/LocalDatabaseHelper.java
@@ -1,28 +1,94 @@
 package org.lightsys.crmapp.data;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+import java.util.ArrayList;
 
 /**
  * Created by Jake on 6/18/2015.
  */
 public class LocalDatabaseHelper extends SQLiteOpenHelper {
-    public LocalDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    //private static LocalDatabaseHelper sInstance;
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "CRMApp.db";
+    public LocalDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(LocalDatabaseContract.SQL_CREATE_ACCOUNT_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(LocalDatabaseContract.SQL_DELETE_ACCOUNT_TABLE);
+        onCreate(db);
+    }
 
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
+    /**
+     * Returns all accounts from the AccountTable in the database. This function should be updated to use an async task.
+     * @return A cursor to all accounts in the accountTable.
+     */
+    public Cursor getAccounts() {
+        //ArrayList<Account> accounts = new ArrayList<Account>();
+        String queryString = "SELECT * FROM " + LocalDatabaseContract.AccountTable.TABLE_NAME;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(queryString, null);
+
+        /** // Note that this is storing and returning the password as plain text which is literally the worst thing.
+        while(c.moveToNext()) {
+            Account account = new Account();
+            account.setId(c.getInt(0));
+            account.setAccountName(c.getString(1));
+            account.setAccountPassword(c.getString(2));
+            account.setServerName(c.getString(3));
+
+            accounts.add(account);
+        } **/
+
+        //c.close();
+        //db.close();
+        return c;
+
+        //return accounts;
     }
 
     public static final class LocalDatabaseContract {
+        public static final String TEXT_TYPE = " TEXT";
+        public static final String INT_TYPE = " INTEGER";
+        public static final String COMMA_SEP = ", ";
+        // Note that this is storing the password in plaintext, which is pretty much the worst idea ever.
+        // However, at this point, getting the app functional with the dev-server is more important.
+        // Also, I'm not sure that I can actually authenticate with Centralix/Kardia without having the password
+        // available.
+        public static final String SQL_CREATE_ACCOUNT_TABLE =
+                "CREATE TABLE " + AccountTable.TABLE_NAME + " (" +
+                AccountTable.COLUMN_ACCOUNT_NAME + TEXT_TYPE + COMMA_SEP +
+                AccountTable.COLUMN_ACCOUNT_PASSWORD + TEXT_TYPE + COMMA_SEP +
+                AccountTable.COLUMN_SERVER_ADDRESS + TEXT_TYPE +
+                ")";
 
+        public static final String SQL_DELETE_ACCOUNT_TABLE = " DROP TABLE IF EXISTS" + AccountTable.TABLE_NAME;
+
+        public static abstract class AccountTable implements BaseColumns {
+            public static final String TABLE_NAME = "accountTable";
+            //public static final String COLUMN_TIMESTAMP = "timestamp";
+            public static final String COLUMN_ACCOUNT_NAME = "accountName";
+            public static final String COLUMN_ACCOUNT_PASSWORD = "accountPassword";
+            public static final String COLUMN_SERVER_ADDRESS = "serverAddress";
+        }
     }
 }
