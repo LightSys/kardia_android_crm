532b8b84aaa3f04452aeb25680eecd5d5e6c0793
jakeprem <jakeprem@gmail.com>
2015-07-16 17:22:17 -0600

    Worked more on 1. I think I'm close to figuring out the basic structure of the Repository, from there I should be able to hammer out the implementation details of that.
    My current solution is not entirely decoupled, however, the only coupled parts would be the REST Implementation and SQLite implementation, both of which are fairly set in stone for Android.
    
    Additionally, this coupling should not introduce problems outside of these 2-3 classes for each repository. (Sidenote: Separate repositories with separate implementations for each does seem slightly repetitive)

--- app/src/main/java/org/lightsys/crmapp/CleanRepository/LoggedInAccount/LoggedInAccountSourceFactory.java
+++ app/src/main/java/org/lightsys/crmapp/CleanRepository/LoggedInAccount/LoggedInAccountSourceFactory.java
@@ -4,6 +4,45 @@ import android.content.Context;
 
 /**
  * Created by Jake on 7/16/2015.
+ * Factory that creates implementations of {@link LoggedInAccountSource}
+ * It should decide whether the account or relevant information
+ * is already in the DataBase or if it needs to go to the REST API
+ * to get the data it needs.
+ *
+ * The LoggedInAccountSource Implementations should handle persisting the data
+ * between each other.
  */
 public class LoggedInAccountSourceFactory {
+
+    private final Context context;
+    // private final UserCache userCache
+    // The example I'm following is using userCache to pass to it's
+    // offline persistence layer, DiskUserDataStore. Not sure yet how that
+    // works for me.
+
+    public LoggedInAccountSourceFactory(Context context) {
+
+        if (context == null) {
+            throw new IllegalArgumentException("Constructor parameters cannot be null");
+        }
+
+        this.context = context;
+    }
+
+    public LoggedInAccountSource create() {
+        LoggedInAccountSource loggedInAccountSource;
+
+        // This shouldn't be if true, it should
+        if (true) {
+            loggedInAccountSource = new SQLiteDataSource();
+        } else {
+            loggedInAccountSource = createRESTApiDataSource();
+        }
+
+        return loggedInAccountSource;
+    }
+
+    private LoggedInAccountSource createRESTApiDataSource() {
+        return null;
+    }
 }
--- app/src/main/java/org/lightsys/crmapp/CleanRepository/LoggedInAccount/RESTApiDataSource.java
+++ app/src/main/java/org/lightsys/crmapp/CleanRepository/LoggedInAccount/RESTApiDataSource.java
@@ -4,4 +4,5 @@ package org.lightsys.crmapp.CleanRepository.LoggedInAccount;
  * Created by Jake on 7/16/2015.
  */
 public class RESTApiDataSource implements LoggedInAccountSource {
+    private final RestApi restApi;
 }
