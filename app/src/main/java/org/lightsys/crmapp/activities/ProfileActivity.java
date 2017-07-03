973982f4d563fcfc0223cbad8f2bd4a5f647eb27
Galhanone <d.dan.gar.g@gmail.com>
2017-07-03 10:50:15 -0600

    Started fixing back button issue

--- app/src/main/java/org/lightsys/crmapp/activities/ProfileActivity.java
+++ app/src/main/java/org/lightsys/crmapp/activities/ProfileActivity.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
